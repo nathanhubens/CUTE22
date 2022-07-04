7919c35c9ddcb7d5f86eba0ea7b9c2d19c2d30d6
Nathan Hubens <nathan.hubens@gmail.com>
2022-07-04 15:54:22 +0200

    update

--- setup.py
+++ setup.py
@@ -6,11 +6,7 @@ setup(
     author="Nathan Hubens",
     author_email="nathan.hubens@umons.ac.be",
     description="A package to convert your Jupyter Notebook",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    url="https://github.com/your_package/homepage/",
     packages=find_packages(),
-    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
