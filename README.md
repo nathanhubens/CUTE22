c80cd494243c033f098ef0e313e3d5d5b87b52b5
Nathan Hubens <nathan.hubens@gmail.com>
2022-07-05 20:38:39 +0200

    add functions

--- README.md
+++ README.md
@@ -5,6 +5,25 @@
 
 ## Results
 
+### Challenge #1
+
+|Team| Accuracy  |   |   |   |
+|---|---|---|---|---|
+| NH  |   |   |   |   |
+|   |   |   |   |   |
+|   |   |   |   |   |
+
+
+### Challenge #2
+
+|Team| Accuracy  |   |   |   |
+|---|---|---|---|---|
+| NH  |   |   |   |   |
+|   |   |   |   |   |
+|   |   |   |   |   |
+
+### Challenge #3
+
 |Team| Accuracy  |   |   |   |
 |---|---|---|---|---|
 | NH  |   |   |   |   |
--- cute/utils.py
+++ cute/utils.py
@@ -18,3 +18,12 @@ def get_activation(activation, name):
     def hook(model, input, output):
         activation[name] = output
     return hook
+
+def count_parameters(model):
+    num_params = sum(p.numel() for p in model.parameters())
+    print(f'Total parameters : {num_params:,}' )
+
+def print_sparsity(model):
+    for k,m in enumerate(model.modules()):
+        if isinstance(m, nn.Conv2d):
+            print(f"Sparsity in {m.__class__.__name__} {k}: {100. * float(torch.sum(m.weight == 0))/ float(m.weight.nelement()):.2f}%")
